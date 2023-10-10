827958f003f5639e57c7614dcdf2a29535a80b44
GUI_nephelo <1736319094@qq.com>
2023-10-10 17:48:10 +0800

    添加了新列型号修复了bug

--- watchDog/db.py
+++ watchDog/db.py
@@ -10,8 +10,9 @@ conn = Elasticsearch(hosts="http://localhost:9200")
 
 def dataFrame2db(df : pd.DataFrame):
     actions = [{"_index":"elec-fence","_id":hs,"_source":data} for hs,data in df.T.to_dict().items()]
+    # print(actions)
     ii,o = bulk(conn,actions)
     logger.info(f"成功推送{ii}条数据")
 
 if __name__=="__main__":
-    bulk(conn,[{"_index":"elec-fence","_id":2,"_source":{"IMSI":1,"IMEI":1,"phoneNum":1,"time":1}}])
+    bulk(conn,[{"_index":"elec-fence","_id":2,"_source":{"IMSI":1,"IMEI":1,"time":1,"phoneNum":None}}])
--- watchDog/main.py
+++ watchDog/main.py
@@ -4,6 +4,7 @@ from watchdog.events import FileSystemEventHandler
 import shutil
 import pandas as pd
 from hashlib import md5
+from time import sleep
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', \
@@ -13,44 +14,72 @@ from db import dataFrame2db
 
 logger = logging.getLogger(__name__)
 
+prefix = "qh_zrt-beijing-"
+
 
 # 要监测的文件夹路径
 folder_path = os.path.expanduser(f"~dataRecv")
 
 class MyEventHandler(FileSystemEventHandler):
     def on_any_event(self, event):
-        # 处理文件变动的逻辑
+        path = event.src_path
+        if event.is_directory:
+            return
+        
+        # print(event.event_type,path)
+        if path.endswith(".tmp") and prefix in path:
+            if event.event_type=="moved":
+                sleep(0.001)
+                path = path[:-4]
+                hold(path)
+            return
         if event.event_type=="closed":
-            path = event.src_path
-            
-            if event.src_path.endswith(".txt") and "qh_zrt-beijing-" in path:
-                logger.info(f"检测到围栏数据{path.split('/')[-1]}")
-                df = trans(path.split('/')[-1])
-                dataFrame2db(df)
-                shutil.move(path,folder_path+"/../backups/")
-            else:
-                logger.info(f"检测到垃圾文件{path.split('/')[-1]}")
-                shutil.move(path,folder_path+"/../junk/")
+            if path.endswith(".txt") and prefix in path:
+                hold(path)
+                return
+            drop(path)
+                
 
+def hold(path):
+    logger.info(f"检测到围栏数据{path.split('/')[-1]}")
+    df = trans(path.split('/')[-1])
+    dataFrame2db(df)
+    shutil.move(path,folder_path+"/../backups/")
+def drop(path):
+    logger.info(f"检测到垃圾文件{path.split('/')[-1]}")
+    # shutil.move(path,folder_path+"/../junk/")
+    os.unlink(path)
 
-col_name = ["IMSI","IMEI","time","phoneNum"]
+col_name = ["IMSI","IMEI","time","phoneNum","model"]
 
 # 将文件夹全部文件进行转换
 def swape():
     dfs = []
     for i in os.listdir(folder_path):
-        dfs.append(trans(i))
-        shutil.move(os.path.join(folder_path,i),folder_path+"/../backups/")
+        path = os.path.join(folder_path,i)
+        if i.endswith(".txt") and prefix in i:
+            dfs.append(trans(i))
+            shutil.move(path,folder_path+"/../backups/")
+        else:
+            # shutil.move(path,folder_path+"/../junk/")
+            os.unlink(path)
     if dfs:
         return pd.concat(dfs)
     return []
+
+def processLine(i):
+    spl = [None if j=="" else j for j in i.split("\t")]
+    for _ in range(len(col_name)-len(spl)): 
+        spl+=[None]
+    return spl
 
 # 转换特定文件
 def trans(fname):
     path = os.path.join(folder_path,fname)
     with open(path,"r") as f:
         lines = f.read().split("\n")[:-1]
-    spaceSegList = [i.split("\t") for i in lines] # 空格分隔的二维表
+    spaceSegList = [i for i in map(processLine,lines)]
+    logger.info(lines)
     index = [md5(i.encode("utf8")).hexdigest() for i in lines] # 以MD5码当作index和es数据库的_id
     df = pd.DataFrame(spaceSegList,columns=col_name,index=index) 
     df["time"] = pd.to_datetime(df["time"], format="%Y%m%d%H%M%S")
@@ -64,6 +93,7 @@ if __name__=="__main__":
     logger.info("程序启动")
     # 清理目录
     data = swape()
+    # print(data)
     if len(data)>0: # 若目录里有东西就转入数据库
         dataFrame2db(data)
     logger.info("清扫完成")
